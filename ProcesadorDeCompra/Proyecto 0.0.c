91c47a7f172c0d78f4b9e13c88e514aba125e82d
Old-Lx <19-10557@gmail.com>
2022-10-21 07:52:22 -0400

    nuevoCliente declaración

--- ProcesadorDeCompra/Proyecto 0.0.c
+++ ProcesadorDeCompra/Proyecto 0.0.c
@@ -1,30 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <csv.h>
+
+/*Declaramos una struct para almacenar fechas*/
+struct fecha {
+    int dia;
+    int mes;
+    int year;
+};
+
+/*Declaramos un struct para guardar clientes*/
+struct cliente {   
+        char nombre[20];
+        char correo[20];
+        char direccion[30];
+        int telefono;
+        char contactof[20];
+        char username[15];
+        char clave[10];
+        struct fecha fechaNacimiento;
+        char lugarNacimiento[20];
+        char genero[10];
+};
+
+/*Arreglo dinámico que guarda a los clientes*/
+struct baseDeDatos {
+    double *datos;
+    size_t size;
+    size_t capacidad;
+};
+
+struct baseDeDatos *bddNueva(size_t N) {
+    struct baseDeDatos *BDD = (struct baseDeDatos *) malloc(sizeof(struct baseDeDatos));
+    BDD->size = N;
+    BDD->capacidad = N;
+    return BDD;
+}
 
 
-void main(){
-    /*Declaramos una struct para almacenar fechas*/
-    struct fecha {
-        int dia;
-        int mes;
-        int year;
-    };
+/*Crea un nuevo cliente*/
+void nuevoCliente() {
     
-    /*Declaramos un struct para guardar clientes*/
-    struct clientes {   
-            char nombre[20];
-            char correo[20];
-            char direccion[30];
-            int telefono;
-            char contactof[20];
-            char username[15];
-            char clave[10];
-            struct fecha fechaNacimiento;
-            char lugarNacimiento[20];
-            char genero[10];
-
-    };
-
-    /*Ahora nos toca definir la función para agregar un nuevo cliente*/
+}
+/*
     struct clientes primero;
     printf("Ingrese día de nacimiento:\n");
     scanf("%d", &primero.fechaNacimiento.dia);
@@ -36,5 +56,5 @@ void main(){
     scanf("%d", &primero.fechaNacimiento.year);
     printf("Ingreso el año: %d", primero.fechaNacimiento.year);
     printf("\n\nExcelente, su fecha de nacimiento es: %d / %d / %d", primero.fechaNacimiento.dia, 
-    primero.fechaNacimiento.mes, primero.fechaNacimiento.year);
-}
+    primero.fechaNacimiento.mes, primero.fechaNacimiento.year);*/
+
