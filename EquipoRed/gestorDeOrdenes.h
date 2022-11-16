75cd58cedb097a6eb2dc23a00fc54642a3817a76
Old-Lx <19-10557@gmail.com>
2022-11-16 11:30:26 -0400

    cambios menores y creacion de gestorDeOrdenes.h

---
+++ EquipoRed/gestorDeOrdenes.h
@@ -0,0 +1,15 @@
+///
+///Gestor de productos
+///
+
+#ifndef _gestorDeOrdenes_h_
+#define _gestorDeOrdenes_h_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "gestorDeOrdenes.h"
+
+
+
+#endif
