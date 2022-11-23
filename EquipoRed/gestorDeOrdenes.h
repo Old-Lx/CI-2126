0bec3e67f7685e42a33bdb182cca707791337340
Old-Lx <19-10557@gmail.com>
2022-11-23 12:49:08 -0400

    gestorDeOrdenes.h actualizado

--- EquipoRed/gestorDeOrdenes.h
+++ EquipoRed/gestorDeOrdenes.h
@@ -37,4 +37,46 @@ int abrirBDDOrdenes() ;
 /*Inicializa un arreglo dinámico de órdenes*/
 DynaOrden *dynaOrden(orden listaDeOrdenes[100]);
 
+/*Devuelve la orden en el indice i*/
+orden valorOrd(int i, const DynaOrden *dynaOrden)
+
+/*Cambia el valor de la orden en el indice i*/
+void dynaCambValOrd(int i, DynaOrden *dynaOrden, orden cambio)
+
+/*Esta funcion devuelve todas las ordenes, sólo usar en modo admin*/
+orden *ordGuardadas(const DynaOrden *dynaOrden)
+
+/*Devuelve la capacidad de la lista de ordenes*/
+int ordCap(const DynaOrden *dynaOrden)
+
+/*Devuelve el tamano de la lista de ordenes*/
+int ordTam(const DynaOrden *dynaOrden)
+
+/*Libera memoria ue contiene arreglo dinamico de ordenes*/
+void remDynaOrden(DynaOrden **dynaOrden)
+
+/*Inserta ordenes desde el indice del ultimo elemento de la lista de ordenes*/
+void dynaInsOrd(DynaOrden *dynaOrden, int N, orden *nueva)
+
+/*Elimina la orden en el indice i de la lista*/
+void elimOrden(DynaOrden *dynaOrden, int pos)
+
+/*Encuentra el indice de la string más cercano a la A*/
+int indOrdAlfa(int i, int j, const char *a[])
+
+/*Cambia la posición de dos ordenes*/
+static void cambOrd(int i, int j, DynaOrden *dynaOrden)
+
+/*Reordena de menor a mayor las ordenes segun su codigo*/
+void sortOrd(DynaOrden *dynaOrden)
+
+/*Por si acaso, función para concatenar dos listas de ordenes*/
+DynaOrden *unirLisOrd(DynaOrden *dynaOrden1, DynaOrden *dynaOrden2)
+
+/*Crea una orden en la base de datos*/
+int crearOrd(orden nuevo)
+
+/*Agrega un producto a la orden determinada*/
+void aggProducto(const char *codigoOrden, producto nuevo, DynaOrden *dynaOrden)
+
 #endif
